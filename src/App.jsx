4cdb2e8c435f76060fafae39eb5278cb37181a11
KRISH KHANNA <krishkhanna00@gmail.com>
2024-08-11 21:00:36 +0530

    made it use NavLink instead of anchor tag and changed how roots are setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import "./index.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Team from "./pages/Team/Team.jsx";
-import Members from "./pages/Members/Members.jsx";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route
-            path="/KaizenClub"
-            element={
-              <>
-                <Home />
-              </>
-            }
-          />
-          <Route path="/KaizenClub/Team" element={<Members />} />
-          <Route path="/KaizenClub/events" element={<Team />} />
-        </Routes>
-      </Router>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
   );
 };
